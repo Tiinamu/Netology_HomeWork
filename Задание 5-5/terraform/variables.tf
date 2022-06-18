7f7df000cf6afbecd1cb023497e84a3a56b6d01d
Tiinamu <95283379+Tiinamu@users.noreply.github.com>
2022-06-18 18:16:21 +0300

    Add files via upload

---
+++ Задание 5-5/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g4u3sfpchj6i21hp7f"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gjl0488dbj7totafg8"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd83ml6qmp4e20u4gr8o"
+}
